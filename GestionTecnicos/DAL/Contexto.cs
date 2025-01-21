7f52f8924b6fd55414ea13b98c43fbbc652fa5f1
erick <erickestradarosario@gmail.com>
2025-01-21 12:27:08 -0400

    Añade DbSet de Clientes

--- GestionTecnicos/DAL/Contexto.cs
+++ GestionTecnicos/DAL/Contexto.cs
@@ -7,12 +7,12 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<Clientes> Clientes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        // Add any additional configurations if needed
+        
         modelBuilder.Entity<Tecnicos>().HasKey(t => t.TecnicoId);
     }
 

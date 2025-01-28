470b804ea855bde8282abf329695b8a6df4059a4
erick <erickestradarosario@gmail.com>
2025-01-28 16:53:17 -0400

    Añade DbSet de Tickets

--- GestionTecnicos/DAL/Contexto.cs
+++ GestionTecnicos/DAL/Contexto.cs
@@ -9,6 +9,8 @@ public class Contexto : DbContext
     public DbSet<Tecnicos> Tecnicos { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
     public DbSet<Ciudades> Ciudades { get; set; }
+    
+    public DbSet<Tickets> Tickets { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

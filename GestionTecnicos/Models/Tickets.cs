4b43a36eebc7e8493927089463328999de7f92dc
erick <erickestradarosario@gmail.com>
2025-01-28 16:52:41 -0400

    Crea modelo Tickets

---
+++ GestionTecnicos/Models/Tickets.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionTecnicos.Models;
+
+
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+    
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+    
+    [Required]
+    [Range(1,4,ErrorMessage = "la prioridad debe ser entre 1 a 4")]
+    public int Prioridad { get; set; }
+    
+    [Required]
+    public int ClienteId { get; set; }
+    
+    [Required]
+    [StringLength(100)]
+    public string Asunto { get; set; }
+    
+    [Required]
+    [StringLength(100)]
+    public string Descripcion { get; set; }
+    
+    public TimeSpan TiempoInvertido { get; set; } = TimeSpan.Zero;
+    
+    public int TecnicoId { get; set; }
+    
+    [ForeignKey("TecnicoId")] 
+    public Tecnicos Tecnico { get; set; } = null!;
+    
+    [ForeignKey("ClienteId")]
+    public Clientes Cliente { get; set; } = null!;
+    
+    //TicketId, Fecha, Prioridad, ClienteId,Asunto, Descripcion, TiempoInvertido, TecnicoId.
+}
